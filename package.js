427d0a52c330a6969ba2d31fd8b4b107472d87eb
Johan Edman <22528843+EdmanJohan@users.noreply.github.com>
2020-10-22 16:44:43 +0200

    Updated package.js

--- package.js
+++ package.js
@@ -1,15 +1,15 @@
 Package.describe({
   name: 'nicolaslopezj:excel-export',
   summary: 'Export data to excel',
-  version: '1.0.0',
-  git: 'https://github.com/nicolaslopezj/excel-export'
+  version: '1.0.2',
+  git: 'https://github.com/EdmanJohan/excel-export'
 });
 
 Npm.depends({
-  'excel-export': '0.4.1'
+  'excel-export-next': '0.4.1'
 });
 
-Package.onUse(function(api) {
+Package.onUse(function (api) {
   api.versionsFrom('1.0');
 
   api.use(['underscore'], 'server');
